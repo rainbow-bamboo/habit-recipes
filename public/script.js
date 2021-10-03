2048401706fede33dd94a38f9f70f45bc7115042
rainbow-bamboo <61608357+rainbow-bamboo@users.noreply.github.com>
2021-10-03 16:41:48 -0400

    places from within a details tag

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-function storeHabit(type,inputID) {
+function storeHabit(type, inputID) {
     const habitBody = document.getElementById(inputID).value
     const storedHabits = window.localStorage.getItem(type)
     let habits = JSON.parse(storedHabits)
@@ -7,8 +7,10 @@ function storeHabit(type,inputID) {
         let stringHabits = JSON.stringify(habits)
         console.log(stringHabits)
         window.localStorage.setItem(type, stringHabits)
+        document.getElementById(inputID).value = ""
     }else{
         window.localStorage.setItem(type, JSON.stringify([{"body": habitBody}]))
+        document.getElementById(inputID).value = ""
     }
 
 }
