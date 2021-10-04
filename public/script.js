dacb5b8aa374a473328dae1bdc85239f3abdf6ae
rainbow-bamboo <61608357+rainbow-bamboo@users.noreply.github.com>
2021-10-04 14:35:00 -0400

    stores habits with incremental ids

--- public/script.js
+++ public/script.js
@@ -3,15 +3,51 @@ function storeHabit(type, inputID) {
     const storedHabits = window.localStorage.getItem(type)
     let habits = JSON.parse(storedHabits)
     if(storedHabits != null){
-        habits.push({"body": habitBody})
+        const newID = maxID(habits) + 1
+        habits.push({"body": habitBody,
+                     "id": newID})
         let stringHabits = JSON.stringify(habits)
         console.log(stringHabits)
         window.localStorage.setItem(type, stringHabits)
         document.getElementById(inputID).value = ""
+        init()
     }else{
-        window.localStorage.setItem(type, JSON.stringify([{"body": habitBody}]))
+        window.localStorage.setItem(type, JSON.stringify([{"body": habitBody,
+                                                            "id": 1}]))
         document.getElementById(inputID).value = ""
+        init()
     }
-
 }
 
+function maxID(habits) {
+    const ids = habits.map(x => x.id)
+    const maxID = Math.max(...ids) // This is the spread operator. It's somehow apply
+    return maxID
+}
+
+function parseFromLocalstorage(key){
+    const stored = JSON.parse(window.localStorage.getItem(key))
+    return stored
+}
+
+function habitsToElements(habits, id){
+    if(habits){
+        let targetElement = document.getElementById(id)
+        targetElement.innerHTML = ""
+        habits.forEach(element => {
+            const newLI = document.createElement("li")
+            const newContent = document.createTextNode(element.body)
+            newLI.appendChild(newContent)
+            targetElement.appendChild(newLI)
+        })
+    }
+}
+
+function init(){
+    const storedIntentions = parseFromLocalstorage('intentions')
+    const storedStacks = parseFromLocalstorage('stacks')
+    habitsToElements(storedIntentions, 'intention-list')
+    habitsToElements(storedStacks, 'stack-list')
+}
+
+init()
